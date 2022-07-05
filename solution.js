c0762061f73a211749c1d022b5fe43d6aabf67f7
chris <chrishethertonweb@gmail.com>
2022-07-05 13:55:02 +0200

    round up done

--- solution.js
+++ solution.js
@@ -1,2 +1,8 @@
 console.log(Math.min(-1, 4));
 console.log(Math.max(-1, 4));
+
+console.log(Math.ceil(3321.32321));
+console.log(Math.ceil(326.76));
+console.log(Math.ceil(76788.7));
+console.log(Math.ceil(-9.78));
+console.log(Math.ceil(43.342));
